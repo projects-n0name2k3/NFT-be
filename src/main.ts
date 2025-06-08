b401f2eeddb80798fdc4bc6d6567f0fdc452fd73
projects-n0name2k3 <nva.tiktok90@gmail.com>
2025-06-09 06:10:42 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   //config cors
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://nft-4lib.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
